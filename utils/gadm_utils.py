b1aaa5f253232c6f943b501c31f03a9c721d5fe6
Olivier Hauterville <hautervo@ist-oar.u-ga.fr>
2024-11-22 14:40:10 +0100

    minor changes

--- utils/gadm_utils.py
+++ utils/gadm_utils.py
@@ -98,7 +98,7 @@ mode = 0 # 0 for parallel, 1 for serial
 data_folder = "/data/mineralogie/hautervo/data/"
 # admin_units = data_folder + "admin_units/world_administrative_boundaries_countries/world-administrative-boundaries.shp"
 admin_units = data_folder + "GADM/ESRI_54009/GADM_0_ESRI54009.shp"
-admin_units = data_folder + "OECD/admin_units/Country/OECD_Country_2020.shp"
+admin_units = data_folder + "OECD/admin_units/Country/OECD_Country_2020_simple1.shp"
 code = "GID_0"
 code = "iso3"
 
