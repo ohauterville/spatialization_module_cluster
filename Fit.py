fec4fa9327570a4fba514fbb1683e020d8b2dd50
Olivier Hauterville <hautervo@ist-oar.u-ga.fr>
2024-11-22 22:10:58 +0100

    minor changes

--- Fit.py
+++ Fit.py
@@ -60,7 +60,7 @@ class Fit:
     def fit_STL(
         self,
         init=[1, 1, 1, 1],
-        bounds=([0, 0, 0, 0], [600, 10000, 1, 100000]),
+        bounds=([0, 0, 0, 0], [600, 10000, 1, 60000]),
     ):
         popt, _ = curve_fit(
             self._STL_objective, [self.x1, self.x2], self.y, p0=init, bounds=bounds
