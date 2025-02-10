886a2a083c15f507244bc2f23f2e96fedd492153
Olivier Hauterville <hautervo@ist-oar.u-ga.fr>
2025-02-10 19:39:26 +0100

    improvements of regional_fit estimator

--- RegionalFit.py
+++ RegionalFit.py
@@ -24,6 +24,8 @@ class RegionalFit:
         self.r_squared = np.nan
 
         self.tolerance_percentage = None
+        self.initial_guess = [50, 0.0003, 18000] 
+        self.bounds = [(10, 250), (0.0001, 0.001), (10000, None)]
         self.nb_vars = 3
         self.colors_reg = ["blue", "orange", "green", "peru", "darkorange", "gold", "purple", "magenta", "chartreuse", "turquoise", "darkcyan", "deepskyblue", "brown",]
 
@@ -101,9 +103,9 @@ class RegionalFit:
     def optimize(self):
         self.define_constraints()
         # Initial guess
-        initial_guess = [50, 0.0003, 18000] * len(self.data_x)
+        initial_guess = self.initial_guess * len(self.data_x)
 
-        bounds = [(10, 250), (0.0001, 0.001), (10000, None)] * len(self.data_x)
+        bounds = self.bounds * len(self.data_x)
 
         # Perform the optimization
         self.opt = minimize(self.objective, initial_guess, bounds=bounds, constraints=self.constraints, method="SLSQP",    options={"maxiter": 10000, "disp": True}
@@ -123,9 +125,12 @@ class RegionalFit:
         # plt.plot(x, national_logistic, color="k", label="national")
 
         for i in range(len(self.pop_distrib)):
-            plt.scatter(self.data_x[i], self.data_y[i], color=self.colors_reg[i], ) #label=f"Data region {i}")
-            plt.plot(self.parent_x, self.logistic(self.parent_x, self.opt.x[self.nb_vars*i+0], self.opt.x[self.nb_vars*i+1], self.opt.x[self.nb_vars*i+2]), color=self.colors_reg[i], linestyle="dashed", label=f"Estimated regional logistic {i}")
-            guessed_logistic += self.pop_distrib[i] * self.logistic(self.parent_x, self.opt.x[self.nb_vars*i+0], self.opt.x[self.nb_vars*i+1], self.opt.x[self.nb_vars*i+2])
+            try:
+                plt.scatter(self.data_x[i], self.data_y[i], color=self.colors_reg[i], ) #label=f"Data region {i}")
+                plt.plot(self.parent_x, self.logistic(self.parent_x, self.opt.x[self.nb_vars*i+0], self.opt.x[self.nb_vars*i+1], self.opt.x[self.nb_vars*i+2]), color=self.colors_reg[i], linestyle="dashed", label=f"Estimated regional logistic {i}")
+                guessed_logistic += self.pop_distrib[i] * self.logistic(self.parent_x, self.opt.x[self.nb_vars*i+0], self.opt.x[self.nb_vars*i+1], self.opt.x[self.nb_vars*i+2])
+            except Exception as e:
+                print(e)
 
         plt.plot(self.parent_x, self.parent_y, color='k', label="parent")
         plt.plot(self.parent_x, guessed_logistic, color="r", label="fit")
